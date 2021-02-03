0d587d863f9351215e9078726468de582ee82ebc
celialiang0627 <celialiang0627@gmail.com>
2021-02-03 00:03:00 -0800

    testing

--- app/src/main/java/com/example/tiktaktoe/MainActivity.java
+++ app/src/main/java/com/example/tiktaktoe/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+// just for test here
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
