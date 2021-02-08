4fcd6e57cd9cf873dca644c51a2fd80521718e06
celialiang0627 <celialiang0627@gmail.com>
2021-02-07 22:32:24 -0800

    add button moves

--- app/src/main/java/com/example/tiktaktoe/MainActivity.java
+++ app/src/main/java/com/example/tiktaktoe/MainActivity.java
@@ -3,8 +3,20 @@ package com.example.tiktaktoe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void dropIn(View view){
+        ImageView counter = (ImageView) view;
+
+        counter.setTranslationY(-1500);
+
+        counter.setImageResource(R.drawable.yellow);
+
+        counter.animate().translationYBy(1500).setDuration(300);
+    }
 // just for test here
     @Override
     protected void onCreate(Bundle savedInstanceState) {
